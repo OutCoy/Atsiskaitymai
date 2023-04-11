6477205a8dffe786dca643543e7c4d8d6d655d37
OutCoy <cizikasg@gmail.com>
2023-04-11 09:22:35 +0300

    4 Uzduotis

--- JS/Task 4/script.js
+++ JS/Task 4/script.js
@@ -10,3 +10,28 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const output = document.querySelector('#output');
+
+fetch(ENDPOINT).then(res => res.json()).then(data => {
+    console.log(data);
+    data.forEach(entry => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const carBrand = document.createElement('h1');
+        const carBrandText = document.createTextNode(entry.brand);
+        carBrand.appendChild(carBrandText);
+
+        const brandModels = document.createElement('ul');
+        entry.models.forEach(model => {
+            const newModel = document.createElement('li');
+            const newModelText = document.createTextNode(model);
+            newModel.appendChild(newModelText);
+
+            brandModels.appendChild(newModel);
+        });
+        card.append(carBrand, brandModels);
+        output.appendChild(card);
+    });
+});
