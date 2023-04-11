ed40f7b3966964e6394577e4ecfc2802f286bad1
OutCoy <cizikasg@gmail.com>
2023-04-11 10:08:13 +0300

    8 Uzduotis

--- JS/Task 8/script.js
+++ JS/Task 8/script.js
@@ -5,3 +5,29 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator{
+    constructor(num1, num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    sum(){
+        return this.num1 + this.num2;
+    }
+
+    subtraction(){
+        return this.num1 - this.num2;
+    }
+
+    multiplication(){
+        return this.num1 * this.num2;
+    }
+
+    division(){
+        return this.num1 / this.num2;
+    }
+}
+
+const test = new Calculator(20, 10);
+console.log(test.sum(), test.subtraction(), test.multiplication(), test.division());
