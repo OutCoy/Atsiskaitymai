f8cff4af9454d87f9bd3c617533e6d63363d28da
OutCoy <cizikasg@gmail.com>
2023-04-11 10:15:10 +0300

    9 Uzduotis

--- JS/Task 9/script.js
+++ JS/Task 9/script.js
@@ -6,3 +6,25 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie{
+    constructor(title, director, budget){
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive(){
+        if(this.budget > 100000000){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+const testFalse = new Movie('Title', 'Director', 100000);
+const testTrue = new Movie('Title', 'Director', 10000000000000);
+
+console.log('False (budget: 100000): ', testFalse.wasExpensive());
+console.log('True (budget: 10000000000000): ', testTrue.wasExpensive());
