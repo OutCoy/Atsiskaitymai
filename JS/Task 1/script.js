b0f06b8a30347373d40957d898ddea3e47ef2935
OutCoy <cizikasg@gmail.com>
2023-04-11 08:35:10 +0300

    1 Uzduotis

--- JS/Task 1/script.js
+++ JS/Task 1/script.js
@@ -8,3 +8,27 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const weightForm = document.querySelector('form');
+const output = document.querySelector('#output');
+
+weightForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const kilograms = e.target.search.value;
+
+    output.innerHTML = "";
+
+    const newPounds = document.createElement('p');
+    const newPoundsText = document.createTextNode(`Pounds: ${kilograms*2.2046}lb`);
+    newPounds.appendChild(newPoundsText);
+
+    const newGrams = document.createElement('p');
+    const newGramsText = document.createTextNode(`Grams: ${kilograms/0.001}g`);
+    newGrams.appendChild(newGramsText);
+
+    const newOunces = document.createElement('p');
+    const newOuncesText = document.createTextNode(`Ounces: ${kilograms*35.274}oz`);
+    newOunces.appendChild(newOuncesText);
+
+    output.append(newPounds, newGrams, newOunces);
+});
